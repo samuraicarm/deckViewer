dae9ab4332e425b6d5dc5d1cdbc93d8f281f3500
Carmen Dukes <carmendukes@gmail.com>
2020-05-21 15:43:08 -0700

    random function added

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const deckUrl = 'https://us.api.blizzard.com/hearthstone/deck/';
 const deckInput = document.getElementById('deckInput');
 const deckForm = document.getElementById('deckForm');
 const defaultDeck = 'AAEBAaIHCLICrwSSlwPBrgOCsQPjtAObtgPLwAMLtAHtAs0DiAePlwP1pwO5rgP+rgOqrwPOrwPDtgMA';
-
+const randomButton = document.getElementById('randomButton');
 
 $(document).ready(function(){
     console.log("ready");
@@ -15,6 +15,7 @@ $(function() {
     console.log('App loaded! Waiting for submit!');
   });
 
+  getDeck(defaultDeck);
 
 function watchForm() {
     console.log("watch form");
@@ -26,6 +27,19 @@ function watchForm() {
     console.log(hearthStoneDeckId );
     });
   }
+
+  function getRandom() {
+    randomButton.addEventListener('click', e => {
+    console.log("I get called after the button is clicked");
+    const arrayIndex = Math.floor(Math.random() * randomHearthstoneDeck.length);
+    const randomHSDeck = randomHearthstoneDeck[arrayIndex].id;
+    console.log(randomHearthstoneDeck[arrayIndex]);
+    e.preventDefault();
+    getDeck(randomHSDeck);
+    });
+  }
+
+  
 
 
 function formatQueryParams(params) {
@@ -64,17 +78,20 @@ fetch(url, options)
 }
 
 function displayResults (responseJson) {
+  console.log(responseJson);
   $('#deckCards').empty();
   for (let i=0; i < responseJson.cards.length; i++){
-    const name = responseJson.cards[i].name.en_us;
-    console.dir(name);
+    const name = responseJson.cards[i].name.en_US;
+    const imageUrl = responseJson.cards[i].image.en_US;
+   
     $('#deckCards').append(
-      <li><h3>${name}</h3></li>
+      `<li><h3>${name}</h3></li>
+      <li><p><img src= "${imageUrl}"></p></li>`
     );
     $('#deckList').removeClass('hidden');
   }
  }
 
 $(watchForm);
-
+$(getRandom);
 
---
+++ random.js
@@ -0,0 +1,62 @@
+let randomHearthstoneDeck = [
+    {
+  deckName: "Basic Druid",
+  id: "AAECAbSKAwAPQ1qdAdUB/gGFA8YEngWFBpoI5AjZCtoKlg2XDQA=",
+  source: "https://hearthstone.gamepedia.com/Basic_deck"
+},
+
+  {
+  deckName: "Basic Hunter",
+  id: "AAECAYoWAA9D2AGBAqQC0wKdA94EiAXtBusHlwjZCtoK+QqXDQA=",
+  source: "https://hearthstone.gamepedia.com/Basic_deck"
+  },
+
+
+  {
+  deckName: "Basic Mage", 
+  id: "AAECAf0EAA9NvwHYAZwCoQK7Ar8DqwS0BPsEngXZCtoK+QqWDQA=",
+  source: "https://hearthstone.gamepedia.com/Basic_deck"
+},
+
+
+  {
+  deckName: "Basic Paladin",  
+  id: "AAECAYsWAA9GnQH6AaMCtALTAv8CnQPuBIgFngXXBfEFmgf5CgA=",
+  source: "https://hearthstone.gamepedia.com/Basic_deck"
+},
+
+{
+    deckName: "Basic Priest", 
+    id: "AAECAa0GAA8pQ1qEAdgBlwKFA6EE4ATlBPsEgAbXCvIMlw0A",
+    source: "https://hearthstone.gamepedia.com/Basic_deck"
+  },
+  
+  {
+    deckName: "Basic Rogue",  
+    id: "AAECAYO6AgAPtAHYAZwCtALTAtkChQOdA8sDzQOLBO4EngXGBZoHAA==",
+    source: "https://hearthstone.gamepedia.com/Basic_deck"
+  },
+  
+  {
+    deckName: "Basic Shaman",  
+    id: "AAECAaoIAA8pM1qVAe8BoQK9A/AD+wSIBf4Fywf0CPkKlg0A",
+    source: "https://hearthstone.gamepedia.com/Basic_deck"
+  },
+  
+
+  {
+    deckName: "Basic Warlock",  
+    id: "AECAf0GAA8wWoQBvwGhAr0DxgTQBKAFyAWSB5cHtgfjB9kKAA==",
+    source: "https://hearthstone.gamepedia.com/Basic_deck"
+  },
+
+  {
+    deckName: "Basic Warrior",  
+    id: "AAECAQcADymdAb8BgQKhAtgCkQOLBPsEgAaRBtAH7wfxB5YNAA==",
+    source: "https://hearthstone.gamepedia.com/Basic_deck"
+  },
+]
+
+
+
+
