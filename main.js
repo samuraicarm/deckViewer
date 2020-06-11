eb0ff3a8052de64a87ae7fce241f0e262860eb30
Carmen Dukes <carmendukes@gmail.com>
2020-06-10 18:30:40 -0700

    copy code added table

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const accessToken = 'USNpaOqf11Sjty1TdzkR781sALs9fIOLSh';
+const accessToken = 'USEI01gXe5TdD7eYxLfmhuzyRWofB6o0Ti';
 const deckUrl = 'https://us.api.blizzard.com/hearthstone/deck/';
 const deckInput = document.getElementById('deckInput');
 const deckForm = document.getElementById('deckForm');
@@ -8,86 +8,85 @@ const defaultDeck = 'AAEBAaIHCLICrwSSlwPBrgOCsQPjtAObtgPLwAMLtAHtAs0DiAePlwP1pwO
 const randomButton = document.getElementById('randomButton');
 let hsDeck = [];
 
-$(document).ready(function(){
-    console.log("ready");
-  });
-  
-  getDeck(defaultDeck);
+getDeck(defaultDeck);
 
 function watchForm() {
-    console.log("watch form");
-    deckForm.addEventListener('submit', e => {
+  console.log("watch form");
+  deckForm.addEventListener('submit', e => {
     console.log("I get called after the form is submitted.");
     e.preventDefault();
     const hearthStoneDeckId = deckInput.value;
     getDeck(hearthStoneDeckId);
     $('#deckForm')[0].reset();
-    console.log(hearthStoneDeckId );
-    });
-  }
+    console.log(hearthStoneDeckId);
+  });
+}
 
-  function getRandom() {
-    randomButton.addEventListener('click', e => {
+function getRandom() {
+  randomButton.addEventListener('click', e => {
     console.log("I get called after the button is clicked");
     const arrayIndex = Math.floor(Math.random() * randomHearthstoneDeck.length);
     const randomHSDeck = randomHearthstoneDeck[arrayIndex].id;
     console.log(randomHearthstoneDeck[arrayIndex]);
     e.preventDefault();
     getDeck(randomHSDeck);
-    });
-  }
+  });
+}
 
 
 
 function formatQueryParams(params) {
-    const queryItems = Object.keys(params)
+  const queryItems = Object.keys(params)
     .map(key => `${key}=${params[key]}`);
-    return queryItems.join('&');
+  return queryItems.join('&');
 }
 
 function getDeck(hearthStoneDeckId) {
-    const params = {
+  const params = {
     locale: 'en_US',
     access_token: accessToken
-    };
-    const queryString = formatQueryParams(params);
-    const url = deckUrl + hearthStoneDeckId + queryString;
-
-    console.log(url);
-
-    const options = {
-    "headers": new Headers({
-      "Authorization": "Bearer USNpaOqf11Sjty1TdzkR781sALs9fIOLSh" })
   };
-   
-    fetch(url, options)
+  const queryString = formatQueryParams(params);
+  const url = deckUrl + hearthStoneDeckId + queryString;
+
+  console.log(url);
+
+  const options = {
+    "headers": new Headers({
+      "Authorization": "Bearer USEI01gXe5TdD7eYxLfmhuzyRWofB6o0Ti"
+    })
+  };
+
+  fetch(url, options)
     .then(response => {
-        if (response.ok){
-            return response.json();
-        }
-        throw new Error(response.statusText);
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
     })
     .then(responseJson => displayResults(responseJson))
-    .catch(err => {$('js-error-message').text(`something went wrong: ${err.message}`);
-  });
+    .catch(err => {
+      $('js-error-message').text(`something went wrong: ${err.message}`);
+    });
 
 }
 
-function displayResults (responseJson) {
+function displayResults(responseJson) {
   console.log(responseJson);
   $('#deckCards').empty();
   const deckClass = responseJson.class.name.en_US;
   const [deckCode, keys] = responseJson.deckCode.split('locale');
   $('#deckCards').append(
-   `<hr><h4>This Hearthstone Deck's class is: <b>${deckClass}</b>.</h4>
-   <input type="text" value=${deckCode} id="codeID"> <button onclick="copyCode()">Copy Deck Code</button>`);
+    `<hr><h4>This Hearthstone Deck's class is: <b>${deckClass}</b>.</h4>
+   <input type="text" value=${deckCode} id="codeID"> <button onclick="copyCode()">Copy Deck Code</button>
+   <span id=“copy_feedback"></span>`);
 
-   hsDeck = responseJson.cards;
-   //console.log(hsDeck);
-  
+  hsDeck = responseJson.cards;
+  //console.log(hsDeck);
 
-   hsDeck.sort((a, b) => {
-   // console.log(a.name.en_US)
+
+  hsDeck.sort((a, b) => {
+    // console.log(a.name.en_US)
     return a.name.en_US.localeCompare(b.name.en_US);
   });
 
@@ -95,16 +94,16 @@ function displayResults (responseJson) {
 
   displayDeckDetails(hsDeck);
 }
-  function displayDeckDetails(hsDeck) {
-    $('.card-container').empty();
-  for (let i=0; i < hsDeck.length; i++){
+function displayDeckDetails(hsDeck) {
+  $('.card-container').empty();
+  for (let i = 0; i < hsDeck.length; i++) {
     const imageUrl = hsDeck[i].image.en_US;
     const name = hsDeck[i].name.en_US;
     const manaCost = hsDeck[i].manaCost;
     const attack = hsDeck[i].attack;
     const health = hsDeck[i].health;
     const deckDescription = hsDeck[i].text.en_US;
-   
+
     if (attack === undefined && health === undefined) {
       $('.card-container').append(
         `<article class="card">
@@ -117,12 +116,11 @@ function displayResults (responseJson) {
           <main class="card-description">
           ${deckDescription}
           </main>
-  
       </article>`
       );
     } else {
-    $('.card-container').append(
-      `<article class="card">
+      $('.card-container').append(
+        `<article class="card">
       <img src= "${imageUrl}" class="thumbnail" alt="${deckDescription}"></figure>
       <header class="card-title">
        <p>${name}</p>
@@ -134,39 +132,40 @@ function displayResults (responseJson) {
         </main>
         <p class = "card-stats"> Attack: ${attack} Health: ${health} </p>
     </article>`
-    );
+      );
     }
   }
-  }
+}
 
 function copyCode() {
   let copyText = document.getElementById("codeID");
   copyText.select();
   copyText.setSelectionRange(0, 99999);
   document.execCommand("copy");
-  alert("Copied the text: " + copyText.value);
+  $("#deckCards").append('<span id=“copy_feedback">Code Copied</span>');
+  //alert("Copied the text: " + copyText.value);
 }
 
 function manaCostArray(hsDeck) {
   let manaCostCards = [];
   hsDeck.sort();
-  for (let i = 0; i < hsDeck.length; i++ ) {
-        manaCostCards.push(hsDeck[i].manaCost);
+  for (let i = 0; i < hsDeck.length; i++) {
+    manaCostCards.push(hsDeck[i].manaCost);
   }
-  
-  //console.log(manaCostCards);
+
+  console.log(manaCostCards);
   manaSpread(manaCostCards);
   return manaCostCards;
-} 
+}
 
 function manaSpread(manaCostCards) {
   let manaCardCount = [];
   // make a copy of the input array
   let copy = manaCostCards.slice(0);
- // console.log('the array has been copied')
+  // console.log('the array has been copied')
   // first loop goes over every element
   for (let i = 0; i < manaCostCards.length; i++) {
-    let myCount = 0;	
+    let myCount = 0;
     // loop over every element in the copy and see if it's the same
     for (let w = 0; w < copy.length; w++) {
       if (manaCostCards[i] == copy[w]) {
@@ -176,7 +175,7 @@ function manaSpread(manaCostCards) {
         delete copy[w];
       }
     }
- 
+
     if (myCount > 0) {
       let a = new Object({});
       a.mana = manaCostCards[i];
@@ -184,25 +183,28 @@ function manaSpread(manaCostCards) {
       manaCardCount.push(a);
     }
   }
-    
-    manaCardCount.sort(function(a,b) {
-      return a.mana-b.mana;
-    });
 
-    console.log(manaCardCount);
-    showSummary(manaCardCount);
+  manaCardCount.sort(function (a, b) {
+    return a.mana - b.mana;
+  });
+
+  console.log(manaCardCount);
+  showSummary(manaCardCount);
 }
 
-function showSummary (manaCardCount){
+function showSummary(manaCardCount) {
   $('#deckSummary').empty();
   $('#deckSummary').append(
-    `<p><b> Mana Cost Summary</b></p>`
+    `<table>
+    <thead>
+    <caption>Summary of Cards</caption>
+    <tr><th>Mana Cost</th><th>Count</th></tr>
+    </thead>
+    </table>`
   );
-  for (const [key,value] of Object.entries(manaCardCount)){
-    $('#deckSummary').append(
-      `<ul>
-      <li> Mana Cost: ${key} Count: ${value.count}</li>
-      </ul>`
+  for (const [key, value] of Object.entries(manaCardCount)) {
+    $('table').append(
+      `<tr><td> ${key} </td><td>${value.count}</td>`
     );
 
   }
